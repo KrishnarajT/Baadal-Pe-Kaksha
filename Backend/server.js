1bf3501511910dd794fd99ab9dcfba1141a3f6f3
sourab777karad <1032211150@mitwpu.edu.in>
2024-02-18 03:41:29 +0530

    added set_appointment functionality

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import AssistantRoutes from './API/Assistant.routes.js';
+import bodyParser from 'body-parser';
 import cors from 'cors';
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
 
 app.use("/assistant", AssistantRoutes);
 app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
